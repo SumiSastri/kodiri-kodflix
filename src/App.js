1bcf5903f32d1d436c838587a27346c4eae68f98
Your Name <sumisastri@gmail.com>
2019-08-08 22:13:36 +0100

    first test - upload image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import './App.css';
 import theHunt from './assets/the-hunt-scandinavian.jpg';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<div>
-					<h1>Kodflix</h1>
-					<img alt="the-hunt-film" src={theHunt} />
-				</div>
-			</header>
+		<div className="container">
+			<div>
+				<h1>Kodflix</h1>
+				<img alt="the-hunt-film" src={theHunt} />
+			</div>
 		</div>
 	);
 }
