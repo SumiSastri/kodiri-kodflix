52890cf1dfaa81498d0d613b2f8a8b7410129760
Your Name <sumisastri@gmail.com>
2019-09-03 00:04:15 +0100

    routing-refactored

--- src/scandi-film-details.js
+++ src/scandi-film-details.js
@@ -6,25 +6,18 @@ export default class ScandiFilmDetails extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			// film: {}
-			welcomeMessage: 'All you ever wanted to know about Scandinavian Noir'
+			film: {}
 		};
 	}
 	componentDidMount() {
 		let filmId = this.props.match.params.filmId;
 		let film = getFilms().find((film) => film.id === filmId);
-		// setTimeout(() => {
-		this.setState({
-			welcomeMessage: 'The best is yet to come...'
-			// film: film
-		});
-		// }, 3000);
+		this.setState({ film });
 	}
 	render() {
 		return (
 			<div className="scandi-film-details">
-				<h1>{this.state.welcomeMessage}</h1>
-				{/* <h1>{this.state.film.name}</h1> */}
+				<h1>{this.state.film.id.name}</h1>
 				<Link to="/">
 					<h4>Page under construction</h4>
 				</Link>
