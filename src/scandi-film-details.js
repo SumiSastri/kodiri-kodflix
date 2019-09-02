99045624afe99730bfdf58072addca4855a20792
Your Name <sumisastri@gmail.com>
2019-09-02 19:42:58 +0100

    rendering state

--- src/scandi-film-details.js
+++ src/scandi-film-details.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// export default function ScandiFilmDetails() {
-// 	return (
-// 		<Link to="/">
-// 			<div className="scandi-film-details">
-// 				<h1>All you ever wanted to know about Scandinavian Noir</h1>
-// 				<h4>Page under construction</h4>
-// 			</div>
-// 		</Link>
-// 	);
-// }
-
 export default class ScandiFilmDetails extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			welcomeMessage: 'All you ever wanted to know about Scandinavian Noir'
+		};
+	}
 	render() {
 		return (
 			<Link to="/">
 				<div className="scandi-film-details">
-					<h1>All you ever wanted to know about Scandinavian Noir</h1>
+					<h1>{this.state.welcomeMessage}</h1>
 					<h4>Page under construction</h4>
 				</div>
 			</Link>
